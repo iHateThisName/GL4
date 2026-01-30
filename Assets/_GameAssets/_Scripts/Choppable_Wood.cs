936c21cf4c9e0fa3a4b5514e836cd7e694486ca9
Einar <einarsterten@gmail.com>
2026-01-30 13:00:20 +0100

    CustomMultiSocket script + prefab, to be used for basket and fireplace

--- Assets/_GameAssets/_Scripts/Choppable_Wood.cs
+++ Assets/_GameAssets/_Scripts/Choppable_Wood.cs
@@ -1,26 +1,48 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 public class Choppable_Wood : MonoBehaviour
 {
     [SerializeField] GameObject prefab1;
     [SerializeField] GameObject prefab2;
-    [SerializeField] AreaTrigger areaTrigger;
 
-    private void OnCollisionEnter(Collision collision)
+    private XRGrabInteractable grabInteractable;
+
+    private void Awake()
     {
-        if (collision.gameObject.CompareTag("Finish"))
-        {
-            // Check if inside the trigger zone
-            if (areaTrigger != null && areaTrigger.IsInside())
-            {
-                // Spawn two prefabs
-                Instantiate(prefab1, transform.position + new Vector3(-0.02f, 0f, 0f), Quaternion.identity);
-                Instantiate(prefab2, transform.position + new Vector3(0.02f, 0f, 0f), Quaternion.identity);
+        grabInteractable = GetComponent<XRGrabInteractable>();
+    }
 
-                areaTrigger.SetInside(false);
-                // Destroy big wood
-                Destroy(gameObject);
+    // Use Trigger instead of Collision for instant detection while held
+    private void OnTriggerEnter(Collider other)
+    {
+        // 1. Check tag
+        if (other.CompareTag("Finish"))
+        {
+            // 2. Ensure it's in the socket
+            if (IsSnappedToSocket())
+            {
+                SpawnPieces();
             }
         }
+    }
+
+    private bool IsSnappedToSocket()
+    {
+        // Check if the wood is currently being held by a socket
+        if (grabInteractable != null && grabInteractable.isSelected)
+        {
+            var interactor = grabInteractable.firstInteractorSelecting;
+            return interactor is UnityEngine.XR.Interaction.Toolkit.Interactors.XRSocketInteractor;
+        }
+        return false;
+    }
+
+    private void SpawnPieces()
+    {
+        Instantiate(prefab1, transform.position + new Vector3(-0.02f, 0f, 0f), transform.rotation);
+        Instantiate(prefab2, transform.position + new Vector3(0.02f, 0f, 0f), transform.rotation);
+
+        Destroy(gameObject);
     }
 }
