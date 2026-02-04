80b17d9364ff4a2430827e0ccf42a32299a0cee0
iHateThisName <ivarravi0@gmail.com>
2026-02-04 13:22:20 +0100

    Implements temperature zone heat effect.
    The GameManager now instantiates the FireAdaptationController at runtime, if one doesn't exist.

--- Assets/_GameAssets/_Scripts/GameManager.cs
+++ Assets/_GameAssets/_Scripts/GameManager.cs
@@ -1,7 +1,21 @@
 using Assets.Scripts.Singleton;
+using Gaskellgames;
 using UnityEngine;
 
 public class GameManager : PersistenSingleton<GameManager> {
+
+    [Header("Prefab Refrences")]
+    [SerializeField, AssetsOnly] private GameObject FireAdaptationVolumePrefab;
+    [HideInInspector] public FireAdaptationController FireAdaptationController { get; private set; }
+
+    private void Start() {
+        // use the existing instance if it exists in the scene
+        this.FireAdaptationController = FindFirstObjectByType<FireAdaptationController>();
+        if (this.FireAdaptationController == null) {
+            // otherwise instantiate a new one from the prefab
+            this.FireAdaptationController = Instantiate(this.FireAdaptationVolumePrefab).GetComponent<FireAdaptationController>();
+        }
+    }
 
     public void ContinueGame() {
         Debug.Log("Continuing Game...");
--- Assets/_GameAssets/_Scripts/TemperatureZoneTrigger.cs
+++ Assets/_GameAssets/_Scripts/TemperatureZoneTrigger.cs
@@ -4,8 +4,40 @@ public class TemperatureZoneTrigger : MonoBehaviour {
 
     [SerializeField] private PlayerTemperatureSimulator.EnumLocationType locationType;
 
+    [ContextMenu("Debug Trigger Zone")]
+    public void DebugTrigger() {
+        Debug.Log($"Debug Trigger Activated for {locationType} zone.");
+        UpdateTemperatureZone();
+    }
     private void OnTriggerEnter(Collider other) {
         if (!other.CompareTag("Player")) return;
+        UpdateTemperatureZone();
+    }
+
+    private void UpdateTemperatureZone() {
+        switch (locationType) {
+            case PlayerTemperatureSimulator.EnumLocationType.Cold:
+                Debug.Log("Player entered COLD temperature zone.");
+
+                // TODO Apply cold vision
+
+                // Remove heating vision
+                GameManager.Instance.FireAdaptationController.RemoveVolume();
+                break;
+            case PlayerTemperatureSimulator.EnumLocationType.Warm:
+                Debug.Log("Player entered WARM temperature zone.");
+
+                // Apply heating vision
+                GameManager.Instance.FireAdaptationController.ApplyVolume();
+                break;
+            case PlayerTemperatureSimulator.EnumLocationType.Normal:
+                Debug.Log("Player entered NORMAL temperature zone.");
+
+                // Remove heating vision
+                GameManager.Instance.FireAdaptationController.RemoveVolume();
+                break;
+        }
+
         PlayerTemperatureSimulator.Instance.SetLocationType(locationType);
     }
 }
