6adf7da8019df2ebba21fae087d537664316d6b6
iHateThisName <ivarravi0@gmail.com>
2026-02-04 11:10:10 +0100

    Firewood documentation.

--- Assets/_GameAssets/_Scripts/Firewood.cs
+++ Assets/_GameAssets/_Scripts/Firewood.cs
@@ -3,7 +3,7 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 [RequireComponent(typeof(XRGrabInteractable))]
 public class Firewood : MonoBehaviour {
-    [field: SerializeField] public float FuelValue { get; private set; } = 20f;
-    public float RemainingFuel;
-    public bool IsBurning = false;
+    [field: SerializeField] public float FuelValue { get; private set; } = 20f; // Total fuel value of this firewood piece.
+    public float RemainingFuel; // Remaining fuel left in this firewood piece will be initialized when added to fireplace.
+    public bool IsBurning = false; // Indicates if this firewood is currently burning and cant be added again to the fireplace.
 }
