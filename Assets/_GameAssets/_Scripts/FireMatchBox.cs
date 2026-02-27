41689852b3e61886239e601200d180af459308a2
Myre36 <67600948+Myre36@users.noreply.github.com>
2026-02-27 14:35:36 +0100

    Commenting code and deleting obsolite code

--- Assets/_GameAssets/_Scripts/FireMatchBox.cs
+++ Assets/_GameAssets/_Scripts/FireMatchBox.cs
@@ -2,48 +2,61 @@ using UnityEngine;
 
 public class FireMatchBox : MonoBehaviour
 {
+    //A refrence to the fire match prefab which will be spawned by the match box
     [SerializeField]
     private GameObject matchPrefab;
 
+    //The spawn point which the match will spawned in
     [SerializeField]
     private Transform matchSpawnPoint;
 
-    public bool matchSpawned = false;
+    //A bool that is supposed to stop the match from duplicating, doesn't work
+    public bool MatchSpawned = false;
 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //Spawn a match at game start
         SpawnMatch();
     }
 
     public void SpawnMatch()
     {
-        if (!matchSpawned)
+        //If there is already a match spawned, a new match should not be spawned
+        if (!MatchSpawned)
         {
+            //Check for if the refrence to the match prefab is missing
             if (matchPrefab == null)
             {
                 Debug.LogError("Match Prefab is missing");
                 return;
             }
 
+            //Check for if the refrence to the match spawn point is missing
             if (matchSpawnPoint == null)
             {
                 Debug.LogError("Match Spawn Point is missing");
                 return;
             }
+
+            //Spawn in a match at the match spawn point
             GameObject newMatch = Instantiate(matchPrefab, matchSpawnPoint.position, matchSpawnPoint.rotation);
+            //Gets a refrence to the match's FireMatchController
             FireMatchController controller = newMatch.GetComponentInChildren<FireMatchController>();
 
+            //Check for if the match doesn't have a FireMatchController
             if (controller == null)
             {
                 Debug.LogError("FireMatchController missing on match prefab!");
                 return;
             }
 
+            //Sets a refrence in the match for this matchbox
             controller.MatchBox = this;
-            matchSpawned = true;
+            //This should make sure that the match doesn't duplicate 
+            MatchSpawned = true;
         }
     }
 }
--- Assets/_GameAssets/_Scripts/FireMatchController.cs
+++ Assets/_GameAssets/_Scripts/FireMatchController.cs
@@ -2,58 +2,56 @@ using System.Collections;
 using UnityEngine;
 
 public class FireMatchController : MonoBehaviour {
+
     [SerializeField] private GameObject rootObject;
     public GameObject RootObject => this.rootObject;
 
+    //A refrence to the fire VFX on the fire match
     [SerializeField] private GameObject fireVFX;
 
+    //The time it takes for the match to despawn
     [SerializeField]
     private float despawnTimer;
 
+    //A refrence to the match's rigidbody
     [SerializeField]
     private Rigidbody rb;
 
+    //A refrence to the match box
     public FireMatchBox MatchBox;
 
-    public bool IsIgnited { get; private set; }
-
-    private void Start() {
-        this.IsIgnited = this.fireVFX.gameObject.activeSelf;
-    }
-
-
-    /*public void Ignite() {
-        if (this.IsIgnited) return; // Already ignited, do nothing
-
-        this.IsIgnited = true;
-        fireVFX.SetActive(true);
-    }*/
-
-    /*public void InstatiateSelf() {
-        //GameObject newFireMatch = Instantiate(this.rootObject, this.transform.position, this.transform.rotation);
-    }*/
-
-    public void EnableModel()
+    //A method for ligthing the match once it is grabbed
+    public void LightMatch()
     {
-        MatchBox.matchSpawned = false;
+        //Make it so that the box knows that the match has been taken
+        MatchBox.MatchSpawned = false;
+        //Make the match affected by physics
         rb.isKinematic = true;
+        //Enabling the VFX for the fire
         fireVFX.SetActive(true);
     }
 
+    //A method for despawning the match once the player releases the match
     public void StartDespawnTimer()
     {
+        //Calling the corroutine that despawns the match after a certain time
         StartCoroutine(DespawnMatch());
-        if (MatchBox.matchSpawned)
+        //Check that is supposed to stop the match from duplicating
+        if (MatchBox.MatchSpawned)
         {
             return;
         }
+        //Making the match box spawning a new match
         MatchBox.SpawnMatch();
     }
 
+    //The coroutine that despawns the match after a set time
     IEnumerator DespawnMatch()
     {
         Debug.Log("Start match despawn");
+        //A timer
         yield return new WaitForSeconds(despawnTimer);
+        //Destroying the match after the time runs out
         Destroy(this.transform.root.gameObject);
     }
 }
