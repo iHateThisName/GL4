63b90d371a6366909d616160a8856e88b2805901
iHateThisName <ivarravi0@gmail.com>
2026-04-14 15:04:41 +0200

    Disable the box collider when it is not necessary

--- Assets/_GameAssets/_Scripts/MonsterSystem/States/Intruder/IntruderIntrudingState.cs
+++ Assets/_GameAssets/_Scripts/MonsterSystem/States/Intruder/IntruderIntrudingState.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 public class IntruderIntrudingState : AnimatedState {
     [SerializeField] private BaseNavAIMonster monsterNavigation;
     [SerializeField] private MonsterSystem.LightSensor lightSensor;
+    [SerializeField] private Collider leftHandCollider;
+    [SerializeField] private Collider rightHandCollider;
 
     private void Awake() {
         if (this.monsterNavigation == null) this.monsterNavigation = this.transform.root.GetComponentInChildren<BaseNavAIMonster>();
         if (this.lightSensor == null) this.lightSensor = this.transform.root.GetComponentInChildren<MonsterSystem.LightSensor>();
+    }
+
+    public override void OnStateEnter() {
+        base.OnStateEnter();
+        this.leftHandCollider.enabled = true;
+        this.rightHandCollider.enabled = true;
     }
     public override void OnAnimationComplete() {
         base.OnAnimationComplete();
