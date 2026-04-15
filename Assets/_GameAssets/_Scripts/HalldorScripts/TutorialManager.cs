1f403d9f96847f786596ca97789eae9f0d9b31c3
Myre36 <67600948+Myre36@users.noreply.github.com>
2026-04-15 14:03:33 +0200

    Some tutorial stuff

--- Assets/_GameAssets/_Scripts/HalldorScripts/TutorialManager.cs
+++ Assets/_GameAssets/_Scripts/HalldorScripts/TutorialManager.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
+    //Bools to see your progress through the night
+    public bool hasLitFire = false;
+    public bool hasEatenFood = false;
+    public bool hasFixedRadio = false;
+
     //A refrence to the night settings
     [SerializeField] private SO_NightSettings nightSettings;
+
+    //A refrence to the temperture manager
+    [SerializeField] private GameObject tempertureManager;
+
+    //A refrence to the hunger manager
+    [SerializeField] private GameObject hungerManager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -12,7 +23,13 @@ public class TutorialManager : MonoBehaviour
         {
             if(nightSettings.DebugStartNight > 1)
             {
-
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                nightSettings.nightTimeMinutes = 0f;
+                Destroy(tempertureManager);
+                Destroy(hungerManager);
             }
         }
     }
