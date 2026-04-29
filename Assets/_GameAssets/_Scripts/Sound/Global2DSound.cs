a33b12aed7ce148afcdd51334eb7a7a687d2095f
iHateThisName <ivarravi0@gmail.com>
2026-04-29 14:29:39 +0200

    Fmod, Chaning to using global paramter instead

--- Assets/_GameAssets/_Scripts/Sound/Global2DSound.cs
+++ Assets/_GameAssets/_Scripts/Sound/Global2DSound.cs
@@ -9,7 +9,7 @@ public class Global2DSound : MonoBehaviour {
     private bool isSprinting = false;
 
     [SerializeField] private string paramterMovementName = "Movement State";
-    [SerializeField] private string paramterWindName = "Wind State";
+    private readonly string globalParamterWindName = "Wind State";
 
     [SerializeField] private UnityEngine.InputSystem.InputActionReference move;
     [SerializeField] private UnityEngine.InputSystem.InputActionReference sprint;
@@ -66,7 +66,7 @@ public class Global2DSound : MonoBehaviour {
     private void HandleMoveInput(InputAction.CallbackContext context) {
         if (context.phase == InputActionPhase.Performed) {
             if (this.isSprinting) {
-                this.emitter.SetParameter(this.paramterMovementName, (int)EnumMovementState.Running);
+                this.emitter.SetParameter(this.paramterMovementName, (int) EnumMovementState.Running);
             } else {
                 this.emitter.SetParameter(this.paramterMovementName, (int)EnumMovementState.Walking);
             }
@@ -78,12 +78,12 @@ public class Global2DSound : MonoBehaviour {
     private void HandleLocationTypeChanged(PlayerTemperatureSimulator.EnumLocationType type) {
         Debug.Log($"Location type changed to {type}");
         if (type == PlayerTemperatureSimulator.EnumLocationType.Normal || type == PlayerTemperatureSimulator.EnumLocationType.Warm) {
-            this.emitter.SetParameter(this.paramterWindName, (int)EnumWindState.Indoor);
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName(this.globalParamterWindName, (float)EnumWindState.Indoor);
         } else if (type == PlayerTemperatureSimulator.EnumLocationType.Cold || type == PlayerTemperatureSimulator.EnumLocationType.Shack) {
-            this.emitter.SetParameter(this.paramterWindName, (int)EnumWindState.Outdoor);
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName(this.globalParamterWindName, (float)EnumWindState.Outdoor);
         }
     }
-    [ContextMenu("Movment Idle")] public void SetMovementIdle() => this.emitter.SetParameter(this.paramterMovementName, (int)EnumMovementState.Idle);
-    [ContextMenu("Movment Walking")] public void SetMovementWalking() => this.emitter.SetParameter(this.paramterMovementName, (int)EnumMovementState.Walking);
-    [ContextMenu("Movment Running")] public void SetMovementRunning() => this.emitter.SetParameter(this.paramterMovementName, (int)EnumMovementState.Running);
+    [ContextMenu("Movment Idle")] public void SetMovementIdle() => FMODUnity.RuntimeManager.StudioSystem.setParameterByName(this.paramterMovementName, (int)EnumMovementState.Idle);
+    [ContextMenu("Movment Walking")] public void SetMovementWalking() => FMODUnity.RuntimeManager.StudioSystem.setParameterByName(this.paramterMovementName, (int)EnumMovementState.Walking);
+    [ContextMenu("Movment Running")] public void SetMovementRunning() => FMODUnity.RuntimeManager.StudioSystem.setParameterByName(this.paramterMovementName, (int)EnumMovementState.Running);
 }
