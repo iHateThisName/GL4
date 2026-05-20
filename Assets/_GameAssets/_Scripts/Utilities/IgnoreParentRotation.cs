b132e966300fa410b3a6d7fe2a316b214322595f
NordBen <bebru0048@student.nord.no>
2026-05-20 15:18:19 +0200

    IgnoreParentUpdate

--- Assets/_GameAssets/_Scripts/Utilities/IgnoreParentRotation.cs
+++ Assets/_GameAssets/_Scripts/Utilities/IgnoreParentRotation.cs
@@ -3,14 +3,41 @@ using UnityEngine;
 public class IgnoreParentRotation : MonoBehaviour
 {
     [SerializeField] private bool ignoreX, ignoreY, ignoreZ;
-    
+    [Range(0, 2)] [Tooltip("0 = Update, 1 = FixedUpdate, 2 = LateUpdate")]
+    [SerializeField] private int typeOfUpdate;
+
+    private Quaternion _lockedRotation;
+
+    private void Start()
+    {
+        _lockedRotation = transform.rotation;
+    }
+
+    private void ApplyIgnore()
+    {
+        Quaternion current = transform.rotation;
+        transform.rotation = Quaternion.Euler(
+            ignoreX ? _lockedRotation.eulerAngles.x : current.eulerAngles.x,
+            ignoreY ? _lockedRotation.eulerAngles.y : current.eulerAngles.y,
+            ignoreZ ? _lockedRotation.eulerAngles.z : current.eulerAngles.z
+        );
+    }
+
     private void Update()
     {
-        Vector3 worldEuler = transform.eulerAngles;
-        transform.rotation = Quaternion.Euler(
-            ignoreX ? 0 : worldEuler.x,
-            ignoreY ? 0 : worldEuler.y,
-            ignoreZ ? 0 : worldEuler.z
-        );
+        if (typeOfUpdate != 0) return;
+        ApplyIgnore();
+    }
+
+    private void FixedUpdate()
+    {
+        if (typeOfUpdate != 1) return;
+        ApplyIgnore();
+    }
+
+    private void LateUpdate()
+    {
+        if (typeOfUpdate != 2) return;
+        ApplyIgnore();
     }
 }
