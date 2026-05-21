39ff539152db7939f2a861d0ea0def41aa772d08
NordBen <bebru0048@student.nord.no>
2026-05-21 10:30:07 +0200

    flashlight socket can select which object it rotates with

--- Assets/_GameAssets/_Scripts/Utilities/FollowXRRotationAndPosition.cs
+++ Assets/_GameAssets/_Scripts/Utilities/FollowXRRotationAndPosition.cs
@@ -2,23 +2,24 @@ using UnityEngine;
 
 public class FollowXRRotationAndPosition : MonoBehaviour
 {
-    [SerializeField] private bool followCameraY = true;
-    [SerializeField] private Transform followPosition;
+    [SerializeField] private bool followYRotation = true;
+    [SerializeField] private Transform followPositionObj;
+    [SerializeField] private Transform followRotationObj;
     [SerializeField] private Vector3 offset = new Vector3(-0.2f, 0.9f, 0.25f);
     
     private Transform parentTransform;
 
     private void Start()
     {
-        this.parentTransform = Camera.main?.transform;
+        this.parentTransform = this.followRotationObj == null ? Camera.main?.transform : this.followRotationObj;
     }
 
     private void LateUpdate()
     {
-        if (this.followPosition != null)
-            this.transform.position = this.followPosition.position + this.offset;
+        if (this.followPositionObj != null)
+            this.transform.position = this.followPositionObj.position + this.offset;
 
-        if (this.followCameraY)
+        if (this.followYRotation)
         {
             float cameraY = this.parentTransform.eulerAngles.y;
             this.transform.rotation = Quaternion.Euler(0f, cameraY, 0f);
