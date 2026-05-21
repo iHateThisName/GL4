d070386c0e2d7fc7f5e23719b144e51de4f10d16
Myre36 <67600948+Myre36@users.noreply.github.com>
2026-05-21 14:18:54 +0200

    Stopping linear velocity

--- Assets/_GameAssets/Scenes/Andreas/PlaceHolders/ItemOutOfAreaScript.cs
+++ Assets/_GameAssets/Scenes/Andreas/PlaceHolders/ItemOutOfAreaScript.cs
@@ -14,6 +14,7 @@ public class ItemOutOfAreaScript : MonoBehaviour
         {
             Debug.Log("Axe should respawn");
             rb.angularVelocity = Vector3.zero;
+            rb.linearVelocity = Vector3.zero;
             axe.transform.position = startPosition.position;
             axe.transform.rotation = startPosition.rotation;
             Debug.Log("Axe should move");
